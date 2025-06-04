109beded1f6595a03308959502c74859484ec7d7
Ariamis613 <Velislav20005@gmail.com>
2025-06-04 19:13:11 +0300

    Made it so that we take 'choice' as user input

--- main.cpp
+++ main.cpp
@@ -1,21 +1,33 @@
 #include "src/App.h"
 
 #include <cassert>
+#include <iostream>
 
- int main(){
+int main(){
     Fitter::Fitter App;
 
     App.Start();
 
-    // Store the exercise data returned from DisplayMenu
-    Fitter::Fitter exerciseData = App.DisplayMenu();
+    std::cin >> App.choice;
 
-    // Copy the exercise data to the main App object
-    App = exerciseData;
+    if(App.choice == 1){
+        std::cout << "=== Exercise Logging ===" << std::endl;
+        Fitter::Fitter exerciseData = App.DisplayMenu();
+        
+        // Copy the exercise data to the main App object
+        App = exerciseData;
+        
+        std::cout << "Exercise logged! Now accessing file manager..." << std::endl;
+    } else if(App.choice == 2){
+        // User chose to go directly to file manager
+        std::cout << "=== File Manager ===" << std::endl;
+    } else {
+        std::cerr << "Invalid choice! Defaulting to file manager..." << std::endl;
+    }
 
     while(App.isRunning){
         App.Update();
     }
 
     return 0;
- }
+}
